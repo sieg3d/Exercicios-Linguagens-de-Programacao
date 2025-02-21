b27a38dff4459b1117d96c1e5afeb6c33987b235
Douglas Lopes <douglas.lopes@soulasalle.com.br>
2025-02-21 20:41:21 -0300

    Update Ex 3.py

--- Lista 3/Ex 3.py
+++ Lista 3/Ex 3.py
@@ -1,5 +1,6 @@
-
 # 3. Tabuada de um Número
+# Peça um número inteiro ao usuário e exiba a tabuada desse número de 1 a 10.
+
 numero_tabuada = int(input("Digite um número para a tabuada: "))
 for i in range(1, 11):
     print(f"{numero_tabuada} x {i} = {numero_tabuada * i}")
