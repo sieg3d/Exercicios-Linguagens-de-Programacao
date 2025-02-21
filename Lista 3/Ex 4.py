3d5a249559bb7451da3f20dca45f34a1580911a7
Douglas Lopes <douglas.lopes@soulasalle.com.br>
2025-02-21 20:41:42 -0300

    Update Ex 4.py

--- Lista 3/Ex 4.py
+++ Lista 3/Ex 4.py
@@ -1,4 +1,8 @@
 # 4. Números Ímpares em um Intervalo
+# Peça ao usuário dois números inteiros, representando um intervalo A,B. O programa deve
+# exibir todos os números ímpares dentro desse intervalo (inclusive os limites, se forem
+# ímpares).
+
 A = int(input("Digite o início do intervalo (A): "))
 B = int(input("Digite o fim do intervalo (B): "))
 for i in range(A, B + 1):
