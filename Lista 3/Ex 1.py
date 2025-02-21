727147f019ae38ea9255b4146f6993d75ba76d5e
Douglas Lopes <douglas.lopes@soulasalle.com.br>
2025-02-21 20:40:15 -0300

    Update Ex 1.py

--- Lista 3/Ex 1.py
+++ Lista 3/Ex 1.py
@@ -1,3 +1,7 @@
+# 1. Contagem Progressiva
+# Escreva um programa que peça um número inteiro positivo ao usuário e exiba todos os
+# números de 1 até esse número, um por linha.
+    
 numero = int(input("Digite um número inteiro positivo: "))
 for i in range(1, numero + 1):
     print(i)
