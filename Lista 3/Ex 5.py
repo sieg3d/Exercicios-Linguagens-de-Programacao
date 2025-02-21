f121f41ecb832d1629f3cae3141b7b35ad7cdd4e
Douglas Lopes <douglas.lopes@soulasalle.com.br>
2025-02-21 20:42:06 -0300

    Update Ex 5.py

--- Lista 3/Ex 5.py
+++ Lista 3/Ex 5.py
@@ -1,4 +1,7 @@
 # 5. Sequência de Fibonacci
+# Escreva um programa que peça ao usuário um número N e exiba os N primeiros termos da
+# Sequência de Fibonacci.
+    
 N = int(input("Digite o número de termos da sequência de Fibonacci: "))
 a, b = 0, 1
 for _ in range(N):
