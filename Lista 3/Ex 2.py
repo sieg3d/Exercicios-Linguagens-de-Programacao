2f5b932e5723cf0964dc16eb6dea33dddf7f02da
Douglas Lopes <douglas.lopes@soulasalle.com.br>
2025-02-21 20:40:50 -0300

    Update Ex 2.py

--- Lista 3/Ex 2.py
+++ Lista 3/Ex 2.py
@@ -1,4 +1,8 @@
 # 2. Soma de Números Positivos
+# Faça um programa que solicite números ao usuário até que ele digite um número negativo.
+# Quando isso acontecer, o programa deve exibir a soma de todos os números positivos
+# digitados e encerrar.
+
 soma = 0
 while True:
     num = int(input("Digite um número (negativo para parar): "))
